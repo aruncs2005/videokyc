bfaa55f6d10b4d719372bb58e14cbf1bd0f33542
Hariharan <sathari@38f9d3c87825.ant.amazon.com>
2020-07-08 15:11:53 +0530

    Revert "add CORS headers to Lambda"
    This reverts commit feffd2524d3a87ce1f27fd3f774124be522fd2c2.

--- amplify/backend/function/facefeatureslambda/src/index.py
+++ amplify/backend/function/facefeatureslambda/src/index.py
@@ -1,13 +1,6 @@
-import json
 def handler(event, context):
   print('received event:')
   print(event)
   return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-        },
-        'body': json.dumps('Hello from Lambda!')
-    };
+    'message': 'Hello from your new Amplify Python lambda!'
+  }
