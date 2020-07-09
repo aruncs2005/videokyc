f7ee3cd0b650cfa23f3acb5e0f1381509a1c1e3a
Hariharan <sathari@38f9d3c87825.ant.amazon.com>
2020-07-09 21:56:04 +0530

    update Upload code

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -7,10 +7,26 @@ import Card from "react-bootstrap/Card"
 class Upload extends React.Component {
     constructor(props) {
       super(props);
+      
+      let documentType;  
+      const documentTypes = [];
+      documentTypes.push({key:"aadhaar", description:"Aadhaar Card"})
+      documentTypes.push({key:"pancard", description:"PAN"})
+      documentTypes.push({key:"passport", description:"Passport"})
+      
+      this.state = {
+            documentType,
+            documentTypes
+        }
+      
     }
+
+
 
     render() {
         
+        // this.state.activeDocument = this.state.documentTypes.find((docType) => docType.key == this.state.documentType );  
+
         return (
             <div>
          <Accordion defaultActiveKey="0">
@@ -25,15 +41,19 @@ class Upload extends React.Component {
                     <Form>
                         <Form.Group controlId="exampleForm.ControlSelect1">
                             <Form.Label>Choose document type</Form.Label>
-                            <Form.Control as="select">
-                            <option>Adhaar Card</option>
-                            <option>PAN</option>
-                            <option>Passport</option>
+                            <Form.Control as="select" value={this.state.documentType}>
+                                {this.state.documentTypes.map((docType, index) =>
+                                    <option selected={this.state.documentType === docType.key} key={index} value={docType.key}>{docType.description}</option>
+                                )}
                             </Form.Control>
                         </Form.Group>
 
                         <Form.Group>
-                            <Form.File id="exampleFormControlFile1" label="Upload the selected ID document" />
+                            <Form.File id="exampleFormControlFile1" label="Upload front page of document" />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.File id="exampleFormControlFile2" label="Upload back page document" />
                         </Form.Group>
 
                         <Button variant="primary" type="submit">
